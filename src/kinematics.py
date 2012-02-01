b84d2ea210a2c3aefaefec9786d8dbb80a93b21d
Rogelio E. Cardona-Rivera <recardona@Normandy.local>
2012-02-01 02:31:15 -0500

    Added kinematics to the Avatar class, and created a kinematics AI module for basic seek/flee behaviors

---
+++ src/kinematics.py
@@ -0,0 +1,17 @@
+#@PydevCodeAnalysisIgnore
+import pygame
+import numpy as np
+
+'''
+    Kinematic Seek Behavior
+'''
+def seek(avatar, target_position, time_passed_seconds):
+    seek_vector = target_position - avatar.position
+    normalized_seek_vector = seek_vector / (np.sqrt(np.sum(np.square(seek_vector))))
+    avatar.move(normalized_seek_vector, time_passed_seconds) 
+    pass
+
+def flee(avatar, target_position, time_passed_seconds):
+    flee_vector = avatar.position - target_position
+    normalized_flee_vector = flee_vector / (np.sqrt(np.sum(np.square(flee_vector))))
+    avatar.move(normalized_flee_vector, time_passed_seconds)
