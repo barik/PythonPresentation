d0fd491f4665a712028260cfda1c1cd90871a38d
Titus Barik <titus@barik.net>
2012-02-01 18:01:48 -0500

    Changed normalization to use dot production.

--- src/kinematics.py
+++ src/kinematics.py
@@ -7,11 +7,11 @@ import numpy as np
 '''
 def seek(avatar, target_position, time_passed_seconds):
     seek_vector = target_position - avatar.position
-    normalized_seek_vector = seek_vector / (np.sqrt(np.sum(np.square(seek_vector))))
+    normalized_seek_vector = seek_vector / np.sqrt(np.dot(seek_vector, seek_vector))
     avatar.move(normalized_seek_vector, time_passed_seconds) 
     pass
 
 def flee(avatar, target_position, time_passed_seconds):
     flee_vector = avatar.position - target_position
-    normalized_flee_vector = flee_vector / (np.sqrt(np.sum(np.square(flee_vector))))
+    normalized_flee_vector = flee_vector / np.sqrt(np.dot(flee_vector, flee_vector))
     avatar.move(normalized_flee_vector, time_passed_seconds)
