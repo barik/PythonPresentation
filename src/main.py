3053035f3d97110750cd2193972eb99773a0e816
Titus Barik <titus@barik.net>
2012-02-01 21:01:31 -0500

    Added frills, such as sound, fonts, and dialog box capabilities.

--- src/main.py
+++ src/main.py
@@ -16,11 +16,73 @@ FPS = 20
 # Until state classes are implemented, state is going to be a global variable.
 state = "INIT"
 
+# Audio tracks; easy to implement. Get tracks at:
+# http://www.nosoapradio.us/
+tracks = [
+    "DST-Greensky.mp3", "DST-DontStop.mp3", "DST-Travel.mp3"
+]
+
+def dialogBox(image, text):
+
+    global screen, tiles, images
+
+    print "Dialog box."
+
+    # The mask is used to dim the screen.
+    maskSurface = pygame.Surface(tiles.get_size())
+    maskSurface.set_alpha(192)
+    maskSurface.fill((0,0,0))
+
+    tiles.blit(maskSurface, (0,0))
+
+    # Now display the dialog box.
+    box = pygame.Surface((500, 150))
+    box.fill((255,255,255))
+    pygame.draw.rect(box, (0,0,0), (0,0,500,10))
+    # pygame.draw.rect(box, (0,0,0), (0,140,500,10))
+
+    box.blit(image, (10,-30))
+
+    font = pygame.font.Font("../res/Jet Set.ttf", 36)
+    text = font.render(text, True, (0,0,0))
+    box.blit(text, (150,50))
+
+    tiles.blit(box, (100,200))
+
+    screen.blit(tiles, (0, 0))
+
+
+    pygame.display.update()
+
+    while pygame.event.wait().type != KEYDOWN: pass
+
+    print "Dialog acknowledged."
+
+
+def backgroundMusic():
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.fadeout(2000)
+    else:
+        pygame.mixer.music.load("../res/" + random.choice(tracks))
+        pygame.mixer.music.play(loops = -1)
+
+def refreshBlit():
+
+    global screen, enemy, player, tiles, world
+
+    tiles = world.renderWorld()
+    screen.fill((0, 0, 0))
+    enemy.blitOn(tiles)
+    player.blitOn(tiles)
+
 def main():
 
     global screen, clock, images
     global player_pos, enemy_pos
-    global tiles
+    global enemy, player
+    global tiles, world
+
+    showDialog = False
 
     # Startup code
     pygame.init()
@@ -41,12 +103,21 @@ def main():
     enemy_pos   = np.array([300,200])
     enemy       = Avatar(world, images["girl"], enemy_pos, 20, True)
 
+    # Initially, background music is playing.
+    backgroundMusic()
+
     # The main game event loop.
     while True:
 
         for event in pygame.event.get():
             if event.type == QUIT:
                 exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_m:
+                    backgroundMusic()
+                if event.key == K_d:
+                    showDialog = True
+
 
         # Handle movement.
         pressed_keys = pygame.key.get_pressed()
@@ -68,14 +139,12 @@ def main():
             acceleration += [0, 1]
 
         if not np.array_equal(acceleration, [0,0]):
-            acceleration = acceleration / np.sqrt(np.dot(acceleration, acceleration))
+            acceleration /=  np.sqrt(np.dot(acceleration, acceleration))
 
         time_passed = clock.tick(FPS)
 
         time_passed_seconds = time_passed / 1000.0
         player.move(acceleration, time_passed_seconds)
-        tiles = world.renderWorld()
-
 
         # You can use this to make left-click exist the game.
         # lmb, mmb, rmb = pygame.mouse.get_pressed()
@@ -90,22 +159,26 @@ def main():
         
         # Seek behavior
         ai.seek(enemy, player.position, time_passed_seconds)
-        # ai.flee(enemy, player.position, time_passed_seconds)
+        #ai.flee(enemy, player.position, time_passed_seconds)
         
         pass
 
+        # Render to intermediate memory buffer.
+        refreshBlit()
 
-        screen.fill((0, 0, 0))
 
-        enemy.blitOn(tiles)
-        player.blitOn(tiles)
+        # If set, show a dialog box.
+        if showDialog:
+            showDialog = False
+            dialogBox(images["girl"], "Tag! You're it!")
+            refreshBlit()
+            dialogBox(images["boy"], "...")
 
+        # Intermediate buffer to screen.
         screen.blit(tiles, (0, 0))
 
         # Update the display, and loop again!
         pygame.display.update()
-
-
 
 def loadImages():
 
